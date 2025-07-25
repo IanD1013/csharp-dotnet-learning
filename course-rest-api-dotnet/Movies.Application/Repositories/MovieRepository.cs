72bd42c81094f4aea13032c7d0a97532b3ef9677
IanD1013 <ian.tung1013@gmail.com>
2025-07-25 18:18:07 +1200

    Add sorting

--- course-rest-api-dotnet/Movies.Application/Repositories/MovieRepository.cs
+++ course-rest-api-dotnet/Movies.Application/Repositories/MovieRepository.cs
@@ -100,7 +100,17 @@ public class MovieRepository : IMovieRepository
     public async Task<IEnumerable<Movie>> GetAllAsync(GetAllMoviesOptions options, CancellationToken token = default)
     {
         using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
-        var result = await connection.QueryAsync(new CommandDefinition("""
+
+        var orderClause = string.Empty;
+        if (options.SortField is not null)
+        {
+            orderClause = $"""
+                           , m.{options.SortField}
+                           order by m.{options.SortField} {(options.SortOrder == SortOrder.Ascending ? "asc" : "desc")}
+                           """;
+        }
+        
+        var result = await connection.QueryAsync(new CommandDefinition($"""
                                                                            select m.*, 
                                                                                   string_agg(distinct g.name, ', ') as genres,
                                                                                   round(avg(r.rating), 1) as rating,
@@ -111,7 +121,7 @@ public class MovieRepository : IMovieRepository
                                                                            left join ratings myr on m.id = myr.movieid and myr.userid = @userid
                                                                            where (@title is null or m.title like ('%' || @title || '%'))
                                                                            and (@yearofrelease is null or m.yearofrelease = @yearofrelease)
-                                                                           group by id, userrating
+                                                                           group by id, userrating { orderClause }
                                                                        """, new { userid = options.UserId, title = options.Title, yearofrelease = options.YearOfRelease }, cancellationToken: token));
         return result.Select(x => new Movie
         {
