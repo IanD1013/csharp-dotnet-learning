b4748029721fa16eb266232e880fd3114f834d7b
IanD1013 <ian.tung1013@gmail.com>
2025-07-27 09:32:52 +1200

    EF - See what goes on in the db

--- course-entity-framework-core/Dometrain.EFCore.Api/Data/MoviesContext.cs
+++ course-entity-framework-core/Dometrain.EFCore.Api/Data/MoviesContext.cs
@@ -10,7 +10,7 @@ public class MoviesContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=MoviesDB;User ID=sa;Password=Wowship2020!;TrustServerCertificate=True");
-        optionsBuilder.LogTo(Console.WriteLine);
+        optionsBuilder.LogTo(Console.WriteLine); // not proper logging but good to see what goes on in the database
         base.OnConfiguring(optionsBuilder);
     }
 }
