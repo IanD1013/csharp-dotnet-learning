badeba6e58bb674c64f0598b89148dbb03952a79
IanD1013 <ian.tung1013@gmail.com>
2025-08-01 09:19:40 +1200

    Clean Architect - Infrastructure Layer - implement the repository pattern in the infrastructure layer & add use case of retrieving a subscription by id

--- course-clean-architecture/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
+++ course-clean-architecture/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
@@ -8,12 +8,12 @@ namespace GymManagement.Application.Subscriptions.Commands.CreateSubscription;
 public class CreateSubscriptionCommandHandler: IRequestHandler<CreateSubscriptionCommand, ErrorOr<Subscription>>
 {
     private readonly ISubscriptionsRepository _subscriptionsRepository;
-    private readonly IUnitOfWork _unitOfWork;
+    // private readonly IUnitOfWork _unitOfWork;
 
-    public CreateSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository, IUnitOfWork unitOfWork)
+    public CreateSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository)
     {
         _subscriptionsRepository = subscriptionsRepository;
-        _unitOfWork = unitOfWork;
+        // _unitOfWork = unitOfWork;
     }
 
     public async Task<ErrorOr<Subscription>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
@@ -21,12 +21,13 @@ public class CreateSubscriptionCommandHandler: IRequestHandler<CreateSubscriptio
         // Create a subscription
         var subscription = new Subscription
         {
-            Id = Guid.NewGuid()
+            Id = Guid.NewGuid(),
+            SubscriptionType = request.SubscriptionType,
         };
         
         // Add it to the database
         await _subscriptionsRepository.AddSubscriptionAsync(subscription);
-        await _unitOfWork.CommitChangesAsync();
+        // await _unitOfWork.CommitChangesAsync();
         
         // Return subscription
         return subscription;
