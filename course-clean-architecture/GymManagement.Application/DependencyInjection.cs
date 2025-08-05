2beefcda99ba139ff6ab84c0c043ece2c84631b7
IanD1013 <ian.tung1013@gmail.com>
2025-08-05 23:21:08 +1200

    Clean Architect - Validation - implement application layer cross-cutting concerns via open generic pipeline behaviors

--- course-clean-architecture/GymManagement.Application/DependencyInjection.cs
+++ course-clean-architecture/GymManagement.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
 using ErrorOr;
+using FluentValidation;
+using GymManagement.Application.Common.Behaviors;
 using GymManagement.Application.Gyms.Commands.CreateGym;
 using GymManagement.Domain.Gyms;
 using MediatR;
@@ -10,11 +12,16 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        // MediatR tells the DI container: Whenever you resolve a request/response pair for IPipelineBehavior<TRequest, TResponse>,
+        // if it matches the generic structure of ValidationBehavior<TRequest, TResponse>, use this class in the pipeline.
         services.AddMediatR(options =>
         {
             options.RegisterServicesFromAssemblyContaining(typeof(DependencyInjection));
-            options.AddBehavior<IPipelineBehavior<CreateGymCommand, ErrorOr<Gym>>, CreateGymCommandBehavior>();
+            // options.AddBehavior<IPipelineBehavior<CreateGymCommand, ErrorOr<Gym>>, CreateGymCommandBehavior>();
+            options.AddOpenBehavior(typeof(ValidationBehavior<,>)); // a ValidationBehavior with two type parameters, but we don’t know what they are yet.
         });
+
+        services.AddValidatorsFromAssemblyContaining(typeof(DependencyInjection));
             
         return services;
     }
