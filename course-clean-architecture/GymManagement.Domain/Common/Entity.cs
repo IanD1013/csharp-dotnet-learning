0ded75c2dd130f8261588727064293203b91c75e
IanD1013 <ian.tung1013@gmail.com>
2025-08-11 23:09:49 +1200

    Clean Architect - Eventual Consistency - implement extracting domain events for offline processing

--- course-clean-architecture/GymManagement.Domain/Common/Entity.cs
+++ course-clean-architecture/GymManagement.Domain/Common/Entity.cs
@@ -5,8 +5,17 @@ public abstract class Entity
     public Guid Id { get; init; }
 
     protected readonly List<IDomainEvent> _domainEvents = new();
-
+    
     protected Entity(Guid id) => Id = id;
+
+    public List<IDomainEvent> PopDomainEvents()
+    {
+        var copy = _domainEvents.ToList();
+        
+        _domainEvents.Clear();
+        
+        return copy;
+    }
 
     protected Entity()
     {
