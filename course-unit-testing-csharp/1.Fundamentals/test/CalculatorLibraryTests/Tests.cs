0c391bfe556d655aa76b6b5339a74eb924846242
IanD1013 <ian.tung1013@gmail.com>
2025-08-15 16:49:33 +1200

    Unit testing fundamentals - write first unit test

---
+++ course-unit-testing-csharp/1.Fundamentals/test/CalculatorLibraryTests/Tests.cs
@@ -0,0 +1,17 @@
+using CalculatorLibrary;
+using Xunit;
+
+namespace CalculatorLibraryTests;
+
+public class Tests
+{
+    [Fact]
+    public void TestAdd()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(1, 2);
+
+        Assert.Equal(3, result);
+    }
+}
