424f459492a55b44bb57a11f3b1e76eef36b0fef
IanD1013 <ian.tung1013@gmail.com>
2025-08-15 17:25:22 +1200

    Unit testing fundamentals - xUnit's execution model

--- course-unit-testing-csharp/1.Fundamentals/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
+++ course-unit-testing-csharp/1.Fundamentals/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
@@ -1,20 +1,42 @@
 using CalculatorLibrary;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace CalculatorLibraryTests;
 
 public class CalculatorTests
 {
+    private readonly Calculator _sut = new();
+    private readonly Guid _guid = Guid.NewGuid();
+
+    private readonly ITestOutputHelper _outputHelper;
+
+    public CalculatorTests(ITestOutputHelper outputHelper)
+    {
+        _outputHelper = outputHelper;
+    }
+
     [Fact]
     public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
     {
-        // Arrange
-        var calculator = new Calculator();
-
         // Act
-        var result = calculator.Add(1, 2);
+        var result = _sut.Add(1, 2);
 
         // Assert
         Assert.Equal(3, result);
     }
+
+    [Fact]
+    public void TestGuid()
+    {
+        _outputHelper.WriteLine(_guid.ToString());
+    }
+
+    [Fact]
+    public void TestGuid2()
+    {
+        _outputHelper.WriteLine(_guid.ToString());
+    }
+
+    // xUnit created a new instance of the test calculator class for each test
 }
