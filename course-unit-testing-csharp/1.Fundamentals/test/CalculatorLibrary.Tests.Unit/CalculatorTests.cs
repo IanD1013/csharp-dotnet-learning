9a89384a535790099ef735d126c50ad548f95f04
IanD1013 <ian.tung1013@gmail.com>
2025-08-15 17:43:25 +1200

    Unit testing fundamentals - parameterizing tests

--- course-unit-testing-csharp/1.Fundamentals/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
+++ course-unit-testing-csharp/1.Fundamentals/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
@@ -4,31 +4,21 @@ using Xunit.Abstractions;
 
 namespace CalculatorLibraryTests;
 
-public class CalculatorTests : IDisposable
+public class CalculatorTests
 {
     private readonly Calculator _sut = new();
-    private readonly ITestOutputHelper _outputHelper;
 
-    public CalculatorTests(ITestOutputHelper outputHelper)
-    {
-        _outputHelper = outputHelper;
-        _outputHelper.WriteLine("Hello from the ctor");
-    }
-
-    [Fact]
-    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
+    [Theory]
+    [InlineData(5, 4, 9)]
+    [InlineData(0, 0, 0)]
+    [InlineData(-5, -4, -9)]
+    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(
+        int a, int b, int expected)
     {
         // Act
-        var result = _sut.Add(1, 2);
+        var result = _sut.Add(a, b);
 
         // Assert
-        Assert.Equal(3, result);
-
-        _outputHelper.WriteLine("Hello from the test itself");
-    }
-
-    public void Dispose()
-    {
-        _outputHelper.WriteLine("Hello from cleanup");
+        Assert.Equal(expected, result);
     }
 }
