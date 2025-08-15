674765403c64489ee99a0a29a316d5a8c61d6729
IanD1013 <ian.tung1013@gmail.com>
2025-08-15 22:18:32 +1200

    Unit testing techniques - writing fluent assertions

--- course-unit-testing-csharp/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
+++ course-unit-testing-csharp/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
@@ -1,4 +1,5 @@
 using CalculatorLibrary;
+using FluentAssertions;
 using Xunit;
 
 namespace CalculatorLibraryTests;
@@ -8,8 +9,6 @@ public class CalculatorTests
     private readonly Calculator _sut = new();
 
     [Theory]
-    [InlineData(5, 4, 9, Skip = "This breaks in CI")]
-    [InlineData(0, 0, 0)]
     [InlineData(-5, -4, -9)]
     public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(
         int a, int b, int expected)
@@ -18,16 +17,7 @@ public class CalculatorTests
         var result = _sut.Add(a, b);
 
         // Assert
-        Assert.Equal(expected, result);
-    }
-
-    [Fact(Skip = "Not implemented yet")]
-    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers()
-    {
-        // Act
-        var result = _sut.Subtract(9, 4);
-
-        // Assert
-        Assert.Equal(5, result);
+        // Assert.Equal(expected, result);
+        result.Should().Be(expected);
     }
 }
