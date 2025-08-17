fb977ec4194f7ce8be07e550a02e73a4b101ee0b
IanD1013 <ian.tung1013@gmail.com>
2025-08-17 20:35:22 +1200

    Advanced unit testing techniques - setting test execution timeouts

---
+++ course-unit-testing-csharp/test/AdvancedTechniques.Tests.Unit/LongRunningTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace AdvancedTechniques.Tests.Unit;
+
+public class LongRunningTests
+{
+    [Fact(Timeout = 2000)]
+    public async Task SlowTest()
+    {
+        await Task.Delay(10000);
+    }
+}
