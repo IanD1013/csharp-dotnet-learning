c36505def04eafe3fcf2c9b5b6e1059b1f32d98f
IanD1013 <ian.tung1013@gmail.com>
2025-08-21 10:55:51 +1200

    Integration testing fundamentals - parameterize tests

--- course-integration-testing-aspdotnetcore/tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
+++ course-integration-testing-aspdotnetcore/tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Net;
 using Xunit;
 
@@ -5,16 +6,49 @@ namespace Customers.Api.Tests.Integration;
 
 public class CustomerControllerTests
 {
+    private readonly HttpClient _httpClient = new()
+    {
+        BaseAddress = new Uri("https://localhost:5001")
+    };
+
     [Fact]
     public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExist()
     {
-        var httpClient = new HttpClient()
-        {
-            BaseAddress = new Uri("https://localhost:5001")
-        };
-
-        var response = await httpClient.GetAsync($"customers/{Guid.NewGuid()}");
+        var response = await _httpClient.GetAsync($"customers/{Guid.NewGuid()}");
 
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Theory]
+    [MemberData(nameof(Data) )]
+    // [ClassData(typeof(classData) )]
+    public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExist2(
+        string guidAsText)
+    {
+        var response = await _httpClient.GetAsync($"customers/{Guid.Parse(guidAsText)}");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    public static IEnumerable<object[]> Data { get; } = new[]
+    {
+        new[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968656" },
+        new[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968657" },
+        new[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968658" }
+    };
+}
+
+public class classData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968656" };
+        yield return new object[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968657" };
+        yield return new object[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968658" };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
     }
 }
