0bf7cbd7f539253ef12102577069e3aab5312396
IanD1013 <ian.tung1013@gmail.com>
2025-08-21 10:28:54 +1200

    Integration testing fundamentals - naming in integration tests

--- course-integration-testing-aspdotnetcore/tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
+++ course-integration-testing-aspdotnetcore/tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -3,10 +3,10 @@ using Xunit;
 
 namespace Customers.Api.Tests.Integration;
 
-public class IntegrationTests
+public class CustomerControllerTests
 {
     [Fact]
-    public async Task Test()
+    public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExist()
     {
         var httpClient = new HttpClient()
         {
