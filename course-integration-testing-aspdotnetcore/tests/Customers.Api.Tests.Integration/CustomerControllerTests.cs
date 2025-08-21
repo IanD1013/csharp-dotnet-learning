dc2d50cd50ca39f5dbc09a21fcdd7dd23b44d69e
IanD1013 <ian.tung1013@gmail.com>
2025-08-21 13:37:15 +1200

    Integration testing concepts and practices - create realistic test data

--- course-integration-testing-aspdotnetcore/tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
+++ course-integration-testing-aspdotnetcore/tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -1,5 +1,8 @@
 using System.Net;
 using System.Net.Http.Json;
+using Bogus;
+using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -10,12 +13,32 @@ namespace Customers.Api.Tests.Integration;
 public class CustomerControllerTests : IClassFixture<WebApplicationFactory<IApiMarker>>
 {
     private readonly HttpClient _httpClient;
+    private readonly Faker<CustomerRequest> _customerGenerator =
+        new Faker<CustomerRequest>()
+            .RuleFor(x => x.FullName, faker => faker.Person.FullName)
+            .RuleFor(x => x.Email, faker => faker.Person.Email)
+            .RuleFor(x => x.GitHubUsername, "nickchapsas")
+            .RuleFor(x => x.DateOfBirth, faker => faker.Person.DateOfBirth.Date);
 
     public CustomerControllerTests(WebApplicationFactory<IApiMarker> appFactory)
     {
         _httpClient = appFactory.CreateClient();
     }
 
+    [Fact]
+    public async Task Create_ReturnsCreated_WhenCustomerIsCreated()
+    {
+        // Arrange
+        var customer = _customerGenerator.Generate();
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync("customers", customer);
+
+        // Assert
+        var customerResponse = await response.Content.ReadFromJsonAsync<CustomerResponse>();
+        customerResponse.Should().BeEquivalentTo(customer);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
 
     [Fact]
     public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExist()
@@ -26,9 +49,6 @@ public class CustomerControllerTests : IClassFixture<WebApplicationFactory<IApiM
         var problem = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
         problem!.Title.Should().Be("Not Found");
         problem.Status.Should().Be(404);
-        // response.Headers.Location!.ToString().Should().Be("...");
-        // response.Headers.GetValues("...");
     }
-
 }
 
