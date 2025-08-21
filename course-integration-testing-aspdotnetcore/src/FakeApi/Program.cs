6623ba782e8583d3affb496fc112559929e706e8
IanD1013 <ian.tung1013@gmail.com>
2025-08-22 09:12:39 +1200

    Integration testing in the real world - create a mocked Api server with WireMock

---
+++ course-integration-testing-aspdotnetcore/src/FakeApi/Program.cs
@@ -0,0 +1,17 @@
+﻿using WireMock.Server;
+using Request = WireMock.RequestBuilders.Request;
+using Response = WireMock.ResponseBuilders.Response;
+
+var wiremockServer = WireMockServer.Start();
+
+Console.WriteLine($"Wiremock is now running on: {wiremockServer.Url}");
+
+wiremockServer.Given(Request.Create()
+        .WithPath("/example")
+        .UsingGet())
+    .RespondWith(Response.Create()
+        .WithBody("This is coming from wiremock")
+        .WithStatusCode(200));
+
+Console.ReadKey();
+wiremockServer.Dispose();
--- course-integration-testing-aspdotnetcore/tests/Customers.Api.Tests.Integration/CustomerApiFactory.cs
+++ course-integration-testing-aspdotnetcore/tests/Customers.Api.Tests.Integration/CustomerApiFactory.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using Xunit;
 
 namespace Customers.Api.Tests.Integration;
@@ -33,6 +34,8 @@ public class CustomerApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifet
                 Password = "changeme",
             }).Build();
 
+    private readonly GitHubApiServer _gitHubApiServer = new();
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureLogging(logging =>
@@ -49,16 +52,28 @@ public class CustomerApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifet
 
             services.AddSingleton<IDbConnectionFactory>(_ =>
                 new NpgsqlConnectionFactory(_dbContainer.ConnectionString));
+
+            services.AddHttpClient("GitHub", httpClient =>
+            {
+                httpClient.BaseAddress = new Uri(_gitHubApiServer.Url);
+                httpClient.DefaultRequestHeaders.Add(
+                    HeaderNames.Accept, "application/vnd.github.v3+json");
+                httpClient.DefaultRequestHeaders.Add(
+                    HeaderNames.UserAgent, $"Course-{Environment.MachineName}");
+            });
         });
     }
 
     public async Task InitializeAsync()
     {
+        _gitHubApiServer.Start();
+        _gitHubApiServer.SetupUser("nickchapsas");
         await _dbContainer.StartAsync();
     }
 
     public new async Task DisposeAsync()
     {
+        _gitHubApiServer.Dispose();
         await _dbContainer.DisposeAsync();
     }
 }
