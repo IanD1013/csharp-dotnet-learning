9179d36e3ea10e2cc27b24daed5264099bf28f2b
IanD1013 <ian.tung1013@gmail.com>
2025-08-21 13:03:36 +1200

    Integration testing concepts and practices - introduce the WebApplicationFactory & testing status codes

--- course-integration-testing-aspdotnetcore/tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
+++ course-integration-testing-aspdotnetcore/tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -1,54 +1,27 @@
-using System.Collections;
 using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 namespace Customers.Api.Tests.Integration;
 
-public class CustomerControllerTests
+public class CustomerControllerTests : IClassFixture<WebApplicationFactory<IApiMarker>>
 {
-    private readonly HttpClient _httpClient = new()
-    {
-        BaseAddress = new Uri("https://localhost:5001")
-    };
+    private readonly HttpClient _httpClient;
 
-    [Fact(Skip = "This doesn't work atm sorry")]
+    public CustomerControllerTests(WebApplicationFactory<IApiMarker> appFactory)
+    {
+        _httpClient = appFactory.CreateClient();
+    }
+
+
+    [Fact]
     public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExist()
     {
         var response = await _httpClient.GetAsync($"customers/{Guid.NewGuid()}");
 
-        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 
-    [Theory]
-    [MemberData(nameof(Data) )]
-    // [ClassData(typeof(classData) )]
-    public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExist2(
-        string guidAsText)
-    {
-        var response = await _httpClient.GetAsync($"customers/{Guid.Parse(guidAsText)}");
-
-        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
-    }
-
-    public static IEnumerable<object[]> Data { get; } = new[]
-    {
-        new[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968656" },
-        new[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968657" },
-        new[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968658" }
-    };
 }
 
-public class classData : IEnumerable<object[]>
-{
-    public IEnumerator<object[]> GetEnumerator()
-    {
-        yield return new object[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968656" };
-        yield return new object[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968657" };
-        yield return new object[] { "22EF1E1A-AE11-43FB-AF55-5B6FFE968658" };
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
-}
