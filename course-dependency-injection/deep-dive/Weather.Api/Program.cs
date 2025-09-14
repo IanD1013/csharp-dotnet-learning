b74e6445626427fcf56d10735ba921dd282fd3c0
IanD1013 <ian.tung1013@gmail.com>
2025-09-14 22:21:33 +1200

    Dependency injection - deep dive - TryAddEnumerable

--- course-dependency-injection/deep-dive/Weather.Api/Program.cs
+++ course-dependency-injection/deep-dive/Weather.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Weather.Api.Logging;
 using Weather.Api.Weather;
 
@@ -14,11 +15,17 @@ builder.Services.AddHttpClient();
 var openWeatherServiceDescriptor = new ServiceDescriptor(typeof(IWeatherService), typeof(OpenWeatherService), ServiceLifetime.Transient);
 var inMemWeatherServiceDescriptor = new ServiceDescriptor(typeof(IWeatherService), typeof(InMemoryWeatherService), ServiceLifetime.Transient);
 
-builder.Services.Add(openWeatherServiceDescriptor);  // 190
-builder.Services.Add(inMemWeatherServiceDescriptor); // 191
+// builder.Services.Add(openWeatherServiceDescriptor);  // 190
+// builder.Services.Add(inMemWeatherServiceDescriptor); // 191
 
 // builder.Services.TryAdd(openWeatherServiceDescriptor);  // 190
 // builder.Services.TryAdd(inMemWeatherServiceDescriptor); // 190
+
+builder.Services.TryAddEnumerable(openWeatherServiceDescriptor);    // 190
+builder.Services.TryAddEnumerable(inMemWeatherServiceDescriptor);   // 191
+builder.Services.TryAddEnumerable(inMemWeatherServiceDescriptor);   // 191
+
+builder.Services.TryAddEnumerable(new [] { openWeatherServiceDescriptor, inMemWeatherServiceDescriptor });   // 191
 
 builder.Services.AddTransient(typeof(ILoggerAdapter<>), typeof(LoggerAdapter<>));
 
