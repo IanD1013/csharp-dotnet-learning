e634c54ecea62c5af31d1572444c596cafd99078
IanD1013 <ian.tung1013@gmail.com>
2025-09-14 21:46:53 +1200

    Dependency injection - deep dive - register open generics

---
+++ course-dependency-injection/deep-dive/Weather.Api/Program.cs
@@ -0,0 +1,28 @@
+using Weather.Api.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
+
+
+builder.Services.AddTransient(typeof(ILoggerAdapter<>), typeof(LoggerAdapter<>));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
