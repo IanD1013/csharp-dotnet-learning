050a85d238620b395cf6a128462af87a37ba0c1b
IanD1013 <ian.tung1013@gmail.com>
2025-09-14 18:20:15 +1200

    Dependency injection - resolving dependencies - from the method

---
+++ course-dependency-injection/resolving-dependencies/WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    [HttpGet("weather")]
+    public IEnumerable<WeatherForecast> Get([FromServices] ILogger<WeatherForecastController> logger)
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
