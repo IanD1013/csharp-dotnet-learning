cd36caf69d1d4906bcf4fc64901910c9542059f8
IanD1013 <ian.tung1013@gmail.com>
2025-09-16 19:55:26 +1200

    Dependency injection - extend dependency injection with scrutor - service registration by scanning

---
+++ course-dependency-injection/scrutor/ScrutorScanning.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ScrutorScanning.ConsoleApp.Services;
+
+var services = new ServiceCollection();
+
+services.Scan(selector =>
+{
+    selector
+        .FromAssemblyOf<Program>()
+        .AddClasses(f => f.InNamespaces("ScrutorScanning.ConsoleApp.Services"))
+        .AsMatchingInterface()
+        .WithSingletonLifetime()
+
+        .AddClasses(f => f.InNamespaces("Services"))
+        .AsMatchingInterface()
+        .WithSingletonLifetime();
+    // .FromAssemblyOf<ExampleAService>()
+    //     .AddClasses()
+});
+
+PrintRegisteredServices(services);
+
+var serviceProvider = services.BuildServiceProvider();
+
+void PrintRegisteredServices(IServiceCollection serviceCollection)
+{
+    foreach (var service in serviceCollection)
+    {
+        Console.WriteLine($"{service.ServiceType.Name} -> {service.ImplementationType?.Name} as {service.Lifetime.ToString()}");
+    }
+}
