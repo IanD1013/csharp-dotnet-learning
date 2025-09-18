6e5adc6e89364ccacf6e3c06bfe86441e9188893
IanD1013 <ian.tung1013@gmail.com>
2025-09-18 12:15:11 +1200

    Dependency injection - create our own DI framework - the design

---
+++ course-dependency-injection/new-di-framework/Consumer.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Consumer.ConsoleApp;
+
+var services = new ServiceCollection();
+
+services.AddSingleton<IConsoleWriter, ConsoleWriter>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var service = serviceProvider.GetRequiredService<IConsoleWriter>();
+
+service.WriteLine("Hello from DI");
