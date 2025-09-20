7d444469b29f073a99e33fc9850df013800703c3
IanD1013 <ian.tung1013@gmail.com>
2025-09-20 23:43:33 +1200

    Logging in dotnet - getting started with logging - the modern .net logger

---
+++ course-logging-in-dotnet/BasicConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+});
+
+ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+
+logger.LogInformation("Hello World!");
