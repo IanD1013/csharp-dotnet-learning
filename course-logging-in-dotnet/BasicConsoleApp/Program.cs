7f96e96b4af3dec3d7c304e5a403203d7af86cb2
IanD1013 <ian.tung1013@gmail.com>
2025-09-21 21:09:00 +1200

    Logging in dotnet - getting started with logging - the log methods

--- course-logging-in-dotnet/BasicConsoleApp/Program.cs
+++ course-logging-in-dotnet/BasicConsoleApp/Program.cs
@@ -8,3 +8,5 @@ using var loggerFactory = LoggerFactory.Create(builder =>
 ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
 
 logger.LogInformation("Hello World!");
+
+// logger.Log(LogLevel.Information, 0,"Hello World!");
