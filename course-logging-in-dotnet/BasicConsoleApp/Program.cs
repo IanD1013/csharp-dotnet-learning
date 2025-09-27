73791ada3d450f7932f31a4134dd3887324ccb21
IanD1013 <ian.tung1013@gmail.com>
2025-09-27 21:12:02 +1200

    Logging in dotnet - deep diving into the logger - customize the loggers and providers

--- course-logging-in-dotnet/BasicConsoleApp/Program.cs
+++ course-logging-in-dotnet/BasicConsoleApp/Program.cs
@@ -1,34 +1,28 @@
-﻿using BasicConsoleApp;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using System.Text.Json;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
 
-using IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureLogging(x =>
-    {
-        x.AddJsonConsole();       
-    })
-    .Build();
-
-var logger = host.Services.GetRequiredService<ILogger<Program>>();
-logger.LogInformation("Hello World!");
-
-host.Run();
-
-/*
-return;
 using var loggerFactory = LoggerFactory.Create(builder =>
 {
-    builder.AddProvider(NullLoggerProvider.Instance);
+    builder.AddJsonConsole(options =>
+    {
+        options.IncludeScopes = false;
+        options.TimestampFormat = "HH:mm:ss ";
+        options.JsonWriterOptions = new JsonWriterOptions
+        {
+            Indented = true
+        };
+    });
 
+    builder.ClearProviders();
+
+    builder.AddSystemdConsole();
+    
+    builder.SetMinimumLevel(LogLevel.Debug);
 });
 
-ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+ILogger logger = loggerFactory.CreateLogger<Program>();
 
 var name = "nick";
 var age = 30;
 
 logger.LogDebug("{Name} just turned: {Age}", name, age);
-logger.LogInformation(LogEvents.UserBirthday,"{Name} just turned: {Age}", name, age);
-*/
