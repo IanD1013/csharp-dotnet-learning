5bde61da593fd92095d851f4664f7636889969c7
IanD1013 <ian.tung1013@gmail.com>
2025-09-30 09:36:10 +1300

    Logging in dotnet - dealing with providers - creating our own logger provider

--- course-logging-in-dotnet/BasicConsoleApp/Program.cs
+++ course-logging-in-dotnet/BasicConsoleApp/Program.cs
@@ -1,29 +1,17 @@
-﻿using Microsoft.ApplicationInsights.Channel;
-using Microsoft.ApplicationInsights.Extensibility;
+﻿using BasicConsoleApp;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-using var channel = new InMemoryChannel();
+IServiceCollection services = new ServiceCollection();
 
-try
+services.AddLogging(x =>
 {
-    IServiceCollection services = new ServiceCollection();
-    services.Configure<TelemetryConfiguration>(x => x.TelemetryChannel = channel);
-    services.AddLogging(x =>
-    {
-        x.AddApplicationInsights(configureTelemetryConfiguration: teleConfig =>
-                teleConfig.ConnectionString = "InstrumentationKey=***REDACTED***",
-            configureApplicationInsightsLoggerOptions: _ => { });
-    });
-    
-    var serviceProvider = services.BuildServiceProvider();
-    
-    var logger =  serviceProvider.GetRequiredService<ILogger<Program>>();
-    
-    logger.LogInformation("Hello World!");
-}
-finally
-{
-    await channel.FlushAsync(CancellationToken.None);
-    await Task.Delay(1000);
-}
+    x.AddProvider(new IanLoggerProvider()); 
+});
+
+var serviceProvider = services.BuildServiceProvider();
+
+var logger =  serviceProvider.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("Hello World!");
+
