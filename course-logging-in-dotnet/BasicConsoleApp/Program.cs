f2e0e1477cb422a119b3fe3d6859086ca0c97058
IanD1013 <ian.tung1013@gmail.com>
2025-09-29 23:55:55 +1300

    Logging in dotnet - dealing with providers - using the application insights provider in a console app

--- course-logging-in-dotnet/BasicConsoleApp/Program.cs
+++ course-logging-in-dotnet/BasicConsoleApp/Program.cs
@@ -1,45 +1,29 @@
-﻿using System.ComponentModel;
-using System.Text.Json;
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
 
-using var loggerFactory = LoggerFactory.Create(builder =>
-{
-    builder.AddJsonConsole(options =>
-    {
-        options.IncludeScopes = false;
-        options.TimestampFormat = "HH:mm:ss ";
-        options.JsonWriterOptions = new JsonWriterOptions
-        {
-            Indented = true
-        };
-    });
-    
-    // builder.SetMinimumLevel(LogLevel.Debug);
-    builder.AddFilter(x => x >= LogLevel.Debug);
-    builder.AddFilter((provider, category, logLevel) =>
-    {
-        return provider!.Contains("Console") &&
-               category!.Contains("Microsoft.Extensions.Hosting.Internal.Host") &&
-               logLevel >= LogLevel.Debug;
-    });
-
-    builder
-        .AddFilter("System", LogLevel.Debug)
-        .AddFilter<ConsoleLoggerProvider>("Microsoft", LogLevel.Information);
-});
-
-ILogger logger = loggerFactory.CreateLogger<Program>();
-
-var name = "nick";
-var age = 30;
+using var channel = new InMemoryChannel();
 
 try
 {
-    throw new Exception("Something went wrong.");
+    IServiceCollection services = new ServiceCollection();
+    services.Configure<TelemetryConfiguration>(x => x.TelemetryChannel = channel);
+    services.AddLogging(x =>
+    {
+        x.AddApplicationInsights(configureTelemetryConfiguration: teleConfig =>
+                teleConfig.ConnectionString = "InstrumentationKey=***REDACTED***",
+            configureApplicationInsightsLoggerOptions: _ => { });
+    });
+    
+    var serviceProvider = services.BuildServiceProvider();
+    
+    var logger =  serviceProvider.GetRequiredService<ILogger<Program>>();
+    
+    logger.LogInformation("Hello World!");
 }
-catch (Exception ex)
+finally
 {
-    logger.LogError(ex,"Failure during birthday of {Name} who is {Age}", name, age);
+    await channel.FlushAsync(CancellationToken.None);
+    await Task.Delay(1000);
 }
-
