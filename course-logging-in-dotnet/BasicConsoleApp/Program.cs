d4888256d6cddca764ee4f5b5735709710a836f0
IanD1013 <ian.tung1013@gmail.com>
2025-10-01 23:13:55 +1300

    Logging in dotnet - advanced logging features - dealing with complex objects

--- course-logging-in-dotnet/BasicConsoleApp/Program.cs
+++ course-logging-in-dotnet/BasicConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Text.Json;
 using Microsoft.Extensions.Logging;
 
 using var loggerFactory = LoggerFactory.Create(builder =>
@@ -8,8 +9,23 @@ using var loggerFactory = LoggerFactory.Create(builder =>
 
 ILogger logger = loggerFactory.CreateLogger<Program>();
 
-var paymentId = 1;
-var amount = 15.99;
-var date = DateTime.Now;
+var paymentData = new PaymentData
+{
+    PaymentId = 1,
+    Amount = 15.99m
+};
 
-logger.LogInformation("New Payment with id {paymentId} for ${Total:C} at {Date:F}", paymentId, amount, date);
+logger.LogInformation("New Payment with Data {PaymentData}", JsonSerializer.Serialize(paymentData));  // New Payment with Data {"PaymentId":1,"Amount":15.99}
+logger.LogInformation("New Payment with Data {PaymentData}", paymentData);  // New Payment with Data PaymentData -> call toString() method
+
+class PaymentData
+{
+    public int PaymentId { get; set; }
+    public decimal Amount { get; set; }
+}
+
+record PaymentData2 // will implement toString()
+{
+    public int PaymentId { get; set; }
+    public decimal Amount { get; set; }
+}
