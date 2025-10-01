2bc779fa1edb67fcf881fa1e97c0cc8a81d66ab6
IanD1013 <ian.tung1013@gmail.com>
2025-10-02 11:41:20 +1300

    Logging in dotnet - advanced logging features - log scopes

--- course-logging-in-dotnet/BasicConsoleApp/Program.cs
+++ course-logging-in-dotnet/BasicConsoleApp/Program.cs
@@ -1,31 +1,33 @@
-﻿
-using System.Text.Json;
+﻿using System.Text.Json;
 using Microsoft.Extensions.Logging;
 
 using var loggerFactory = LoggerFactory.Create(builder =>
 {
-    builder.AddConsole();
+    builder.AddJsonConsole(x =>
+    {
+        x.IncludeScopes = true;
+        x.JsonWriterOptions = new JsonWriterOptions
+        {
+            Indented = true
+        };
+    });
 });
 
 ILogger logger = loggerFactory.CreateLogger<Program>();
 
-var paymentData = new PaymentData
-{
-    PaymentId = 1,
-    Amount = 15.99m
-};
+var paymentId = 1;
+var amount = 15.99;
 
-logger.LogInformation("New Payment with Data {PaymentData}", JsonSerializer.Serialize(paymentData));  // New Payment with Data {"PaymentId":1,"Amount":15.99}
-logger.LogInformation("New Payment with Data {PaymentData}", paymentData);  // New Payment with Data PaymentData -> call toString() method
-
-class PaymentData
+using (logger.BeginScope("{PaymentId}", paymentId))
+using (logger.BeginScope("{TotalAmount}", amount))
+    
+try
 {
-    public int PaymentId { get; set; }
-    public decimal Amount { get; set; }
+    logger.LogInformation("New payment for ${Total}", amount);
+    // processing 
 }
-
-record PaymentData2 // will implement toString()
+finally
 {
-    public int PaymentId { get; set; }
-    public decimal Amount { get; set; }
+    logger.LogInformation("Payment processing completed");
 }
+  
