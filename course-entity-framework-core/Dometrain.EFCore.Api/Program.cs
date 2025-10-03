bf92e247a548e91e051d95529412d4298a581708
IanD1013 <ian.tung1013@gmail.com>
2025-10-04 11:25:15 +1300

    EF - transform the db schema with migrations - deal with an out-of-sync model

--- course-entity-framework-core/Dometrain.EFCore.Api/Program.cs
+++ course-entity-framework-core/Dometrain.EFCore.Api/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 using Dometrain.EFCore.Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,8 +17,9 @@ var app = builder.Build();
 // DIRTY HACK, WILL COME BACK TO FIX THIS:
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<MoviesContext>();
-context.Database.EnsureDeleted();
-context.Database.EnsureCreated();
+var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
+if (pendingMigrations.Any())
+    throw new Exception("Database is not fully migrated for MoviesContext.");
 
 if (app.Environment.IsDevelopment())
 {
