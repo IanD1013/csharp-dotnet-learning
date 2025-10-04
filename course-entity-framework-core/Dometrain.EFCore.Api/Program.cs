a4e828d9bb846fcd998d0cf357f608330816a849
IanD1013 <ian.tung1013@gmail.com>
2025-10-04 23:26:47 +1300

    EF - using EF Core in a bigger solution - configure logging

--- course-entity-framework-core/Dometrain.EFCore.Api/Program.cs
+++ course-entity-framework-core/Dometrain.EFCore.Api/Program.cs
@@ -1,12 +1,21 @@
 using System.Text.Json.Serialization;
 using Dometrain.EFCore.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers()
     .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())); // adds a converter so that enums are serialized as strings instead of numbers.
 
+// Configure Serilog
+var serilog = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Services.AddSerilog(serilog);
+
+// Add Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,8 +24,9 @@ builder.Services.AddDbContext<MoviesContext>(optionsBuilder =>
 {
     var connectionString = builder.Configuration.GetConnectionString("MoviesContext");
     optionsBuilder
-        .UseSqlServer(connectionString)
-        .LogTo(Console.WriteLine);
+        .UseSqlServer(connectionString);
+    // .EnableSensitiveDataLogging()
+    // .LogTo(Console.WriteLine);
 },
 ServiceLifetime.Scoped, // service lifetime of the db context
 ServiceLifetime.Singleton); // when is our db context options changing
@@ -24,11 +34,13 @@ ServiceLifetime.Singleton); // when is our db context options changing
 var app = builder.Build();
 
 // Check if the DB was migrated: 
-var scope = app.Services.CreateScope();
-var context = scope.ServiceProvider.GetRequiredService<MoviesContext>();
-var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
-if (pendingMigrations.Any())
-    throw new Exception("Database is not fully migrated for MoviesContext.");
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<MoviesContext>();
+    var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
+    if (pendingMigrations.Any())
+        throw new Exception("Database is not fully migrated for MoviesContext.");
+}
 
 if (app.Environment.IsDevelopment())
 {
