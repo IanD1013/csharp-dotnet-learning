0f7e71e18013e493cd18c9e547521c069d20d9c3
IanD1013 <ian.tung1013@gmail.com>
2025-10-05 23:08:24 +1300

    Logging in dotnet - Serilog - the concept of sinks

--- course-logging-in-dotnet/SerilogConsoleApp/Program.cs
+++ course-logging-in-dotnet/SerilogConsoleApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 
 ILogger logger = new LoggerConfiguration()
+    .WriteTo.Console(theme:AnsiConsoleTheme.Code)
+    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
     .CreateLogger();
     
 logger.Information("Hello World!");
