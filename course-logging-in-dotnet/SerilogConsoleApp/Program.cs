02879fb70809d4ad2b3e1d9249af1ca1e3040795
IanD1013 <ian.tung1013@gmail.com>
2025-10-05 23:16:44 +1300

    Logging in dotnet - Serilog - the static serilog logger

--- course-logging-in-dotnet/SerilogConsoleApp/Program.cs
+++ course-logging-in-dotnet/SerilogConsoleApp/Program.cs
@@ -3,7 +3,11 @@ using Serilog.Sinks.SystemConsole.Themes;
 
 ILogger logger = new LoggerConfiguration()
     .WriteTo.Console(theme:AnsiConsoleTheme.Code)
-    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
+    // .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
     .CreateLogger();
+
+Log.Logger = logger;
     
 logger.Information("Hello World!");
+
+Log.CloseAndFlush();
