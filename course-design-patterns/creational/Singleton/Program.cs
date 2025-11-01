fd1a904569ddc2757275e81612dbca4f31590581
IanD1013 <ian.tung1013@gmail.com>
2025-11-02 09:53:13 +1300

    Creational - singleton - naive singleton implementation with lazy loading

--- course-design-patterns/creational/Singleton/Program.cs
+++ course-design-patterns/creational/Singleton/Program.cs
@@ -1,12 +1,27 @@
-﻿Singleton singleton1 = Singleton.Instance;
+﻿Console.WriteLine("Before accessing instance");
+Singleton singleton1 = Singleton.Instance;
+Console.WriteLine("After accessing instance");
 Singleton singleton2 = Singleton.Instance;
 
 sealed class Singleton
 {
-    public static Singleton Instance { get; } = new();
+    private static Singleton _instance = null!;
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance is null)
+            {
+                _instance = new Singleton();
+            }
+            
+            return _instance;
+        }
+    } 
     
     private Singleton()
     {
-        
+        Console.WriteLine("Instantiating singleton");
     }
 }
