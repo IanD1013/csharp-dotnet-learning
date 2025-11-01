c8353df35ee150edbdfb4fcea3f2ed4715d11267
IanD1013 <ian.tung1013@gmail.com>
2025-11-01 20:32:14 +1300

    Logging in dotnet - high performance logging - the LoggerMessage.Define method

---
+++ course-logging-in-dotnet/HighPerformanceLoggingConsoleApp/PaymentService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace HighPerformanceLoggingConsoleApp;
+
+public class PaymentService
+{
+    private readonly ILogger<PaymentService> _logger;
+    
+    private static readonly Action<ILogger, string, decimal, int, Exception?> _logPayment =
+        LoggerMessage.Define<string, decimal, int>(
+            LogLevel.Information, 
+            new EventId(5001, nameof(CreatePayment)), 
+            "Customer {Email} has paid {Amount} for product {ProductId}"
+            );
+
+    public PaymentService(ILogger<PaymentService> logger)
+    {
+        _logger = logger;
+    }
+
+    public void CreatePayment(string email, decimal amount, int productId)
+    {
+        // Do some work
+        // _logger.LogInformation("Customer {Email} has paid {Amount} for product {ProductId}", email, amount, productId);
+        _logPayment(_logger, email, amount, productId, null);
+    }
+}
