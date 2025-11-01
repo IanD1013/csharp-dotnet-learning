791ed7e624cdfa425a40a22d99f8628d6a24a635
IanD1013 <ian.tung1013@gmail.com>
2025-11-02 09:42:44 +1300

    Creational - singleton - naive singleton implementation

---
+++ course-design-patterns/creational/Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿Singleton singleton1 = Singleton.Instance;
+Singleton singleton2 = Singleton.Instance;
+
+sealed class Singleton
+{
+    public static Singleton Instance { get; } = new();
+    
+    private Singleton()
+    {
+        
+    }
+}
