0c8cc9771bfff43512491544cc4b916fd4e8bf5b
IanD1013 <ian.tung1013@gmail.com>
2025-11-02 16:03:08 +1300

    Intro - first configuration nuget package

---
+++ course-configuration-and-options/1-Intro/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder();
+
+builder.AddInMemoryCollection([
+    new KeyValuePair<string, string?>("Key", "123 that was easy...")
+]);
+
+var config = builder.Build();
+
+var value = config["Key"];
+
+Console.WriteLine(value);
