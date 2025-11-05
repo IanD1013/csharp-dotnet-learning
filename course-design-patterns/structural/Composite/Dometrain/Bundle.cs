8ab4e4a7a76d03b9cb7f84baf19cca4aa4fbdf65
IanD1013 <ian.tung1013@gmail.com>
2025-11-06 10:58:17 +1300

    Structural - composite - implement the dometrain example

---
+++ course-design-patterns/structural/Composite/Dometrain/Bundle.cs
@@ -0,0 +1,36 @@
+﻿namespace Composite.Dometrain;
+
+public class Bundle(string name) : LearningResource
+{
+    private readonly List<LearningResource> _children = [];
+    
+    public override decimal GetPrice()
+    {
+        return _children.Sum(x => x.GetPrice()) * 0.8m;   
+    }
+
+    public override TimeSpan GetDuration()
+    {
+        return new TimeSpan(_children.Sum(x => x.GetDuration().Ticks));
+    }
+
+    public override string GetName()
+    {
+        return name;   
+    }
+    
+    public override void Add(LearningResource learningResource)
+    {
+        _children.Add(learningResource);
+    }
+    
+    public override void Remove(LearningResource learningResource)
+    {
+        _children.Remove(learningResource);
+    }
+
+    public override LearningResource? GetLearningResource(string nameParam)
+    {
+        return _children.SingleOrDefault(x => x.GetName() == nameParam);  
+    }
+}
--- course-design-patterns/structural/Composite/Program.cs
+++ course-design-patterns/structural/Composite/Program.cs
@@ -1,18 +1,45 @@
 ﻿using Composite;
+using Composite.Dometrain;
 using ComponentClass = Composite.Component;
 using CompositeClass = Composite.Composite;
 
-ComponentClass root = new CompositeClass();
-ComponentClass leafA = new Leaf();
+#region CompositePattern
+
+// ComponentClass root = new CompositeClass();
+// ComponentClass leafA = new Leaf();
+// root.Add(leafA);
+//
+// ComponentClass childComposite = new CompositeClass();
+// ComponentClass leafB = new Leaf();
+// ComponentClass leafC = new Leaf();      
+// childComposite.Add(leafB);  
+// childComposite.Add(leafC);
+//
+// root.Add(childComposite);
+//
+// root.Operation();
+// leafA.Operation();
+
+#endregion
+
+
+#region DometrainExample
+
+LearningResource root = new Bundle(name: "Zero to Hero: Clean Architecture");
+
+LearningResource leafA = new Course(
+    name: "Getting Started: Clean Architecture",
+    duration: TimeSpan.FromHours(3),
+    price: 100);
+
+LearningResource leafB = new Course(
+    name: "Deep Dive: Clean Architecture",
+    duration: TimeSpan.FromHours(4),
+    price: 110);
+
 root.Add(leafA);
+root.Add(leafB);
 
-ComponentClass childComposite = new CompositeClass();
-ComponentClass leafB = new Leaf();
-ComponentClass leafC = new Leaf();      
-childComposite.Add(leafB);  
-childComposite.Add(leafC);
+Console.WriteLine(root.GetPrice());
 
-root.Add(childComposite);
-
-root.Operation();
-leafA.Operation();
+#endregion
