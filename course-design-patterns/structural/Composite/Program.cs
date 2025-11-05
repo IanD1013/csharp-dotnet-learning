d284e43f84f2d1b0f1ddf52e0a56a815e1743c3f
IanD1013 <ian.tung1013@gmail.com>
2025-11-06 10:13:50 +1300

    Structural - composite - implement the composite pattern

---
+++ course-design-patterns/structural/Composite/Program.cs
@@ -0,0 +1,18 @@
+﻿using Composite;
+using ComponentClass = Composite.Component;
+using CompositeClass = Composite.Composite;
+
+ComponentClass root = new CompositeClass();
+ComponentClass leafA = new Leaf();
+root.Add(leafA);
+
+ComponentClass childComposite = new CompositeClass();
+ComponentClass leafB = new Leaf();
+ComponentClass leafC = new Leaf();      
+childComposite.Add(leafB);  
+childComposite.Add(leafC);
+
+root.Add(childComposite);
+
+root.Operation();
+leafA.Operation();
