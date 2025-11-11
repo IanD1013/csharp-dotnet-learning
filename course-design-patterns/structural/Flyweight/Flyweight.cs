f012d8e1ba25e9e99466ac120737be618b8e2577
IanD1013 <ian.tung1013@gmail.com>
2025-11-11 14:18:40 +1300

    Structural - flyweight - implement the flyweight design pattern

---
+++ course-design-patterns/structural/Flyweight/Flyweight.cs
@@ -0,0 +1,6 @@
+﻿namespace Flyweight;
+
+public interface Flyweight
+{
+    public void Operation(string extrinsicState);
+}
---
+++ course-design-patterns/structural/Flyweight/Program.cs
@@ -0,0 +1,8 @@
+﻿using Flyweight;
+
+FlyweightFactory factory = new FlyweightFactory();
+Client client = new Client(factory);
+
+client.Operation("Hello");
+client.Operation("Hi");
+client.Operation("World");
