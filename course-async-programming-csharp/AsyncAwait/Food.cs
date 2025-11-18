3233b652e5d29a08e085fc18ecd8133e0a9e0c80
IanD1013 <ian.tung1013@gmail.com>
2025-11-18 15:26:12 +1300

    Async programming - async/await - life before async await

---
+++ course-async-programming-csharp/AsyncAwait/Food.cs
@@ -0,0 +1,25 @@
+﻿namespace AsyncAwait;
+
+public abstract class Food
+{
+    readonly TimeSpan _cookTime;
+    public string Name { get; }
+
+    protected Food(TimeSpan cookTime)
+    {
+        _cookTime = cookTime;
+        Name = GetType().Name;
+    }
+    
+    public async Task Cook()
+    {
+        Console.WriteLine($"Cooking {Name}");
+        await Task.Delay(_cookTime);
+        Console.WriteLine($"{Name} Completed");
+    }
+}
+
+public class Turkey() : Food(TimeSpan.FromSeconds(5));
+public class MashedPotatoes() : Food(TimeSpan.FromSeconds(2));
+public class Gravy() : Food(TimeSpan.FromSeconds(1));
+public class Stuffing() : Food(TimeSpan.FromSeconds(2));
---
+++ course-async-programming-csharp/AsyncAwait/Program.cs
@@ -0,0 +1,26 @@
+﻿using AsyncAwait;
+
+Console.WriteLine("Cooking Started");
+
+#region FirstExample
+var turkey = new Turkey();
+turkey.Cook();
+#endregion
+
+
+#region Callbacks
+turkey.Cook()
+    .ContinueWith(_ =>
+    {
+        var gravy = new Gravy();
+        gravy.Cook()
+            .ContinueWith(_ =>
+            {
+                Console.WriteLine("Gravy Completed");
+                Console.WriteLine("Ready to eat");
+            });
+        
+    });
+#endregion
+
+Console.ReadLine();
