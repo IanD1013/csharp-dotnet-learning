ff00e4216a42c790d36acde530ea16a9687457a9
IanD1013 <ian.tung1013@gmail.com>
2025-11-18 15:40:32 +1300

    Async programming - async/await - using async

--- course-async-programming-csharp/AsyncAwait/Program.cs
+++ course-async-programming-csharp/AsyncAwait/Program.cs
@@ -1,26 +1,45 @@
 ﻿using AsyncAwait;
 
+// Console.WriteLine("Cooking Started");
+
+// #region FirstExample
+// var turkey = new Turkey();
+// turkey.Cook();
+// #endregion
+//
+//
+// #region Callbacks
+// turkey.Cook()
+//     .ContinueWith(_ =>
+//     {
+//         var gravy = new Gravy();
+//         gravy.Cook()
+//             .ContinueWith(_ =>
+//             {
+//                 Console.WriteLine("Gravy Completed");
+//                 Console.WriteLine("Ready to eat");
+//             });
+//         
+//     });
+// #endregion
+
+// Console.ReadLine();
+
+#region UsingAsync
+
 Console.WriteLine("Cooking Started");
+Console.WriteLine("Cooking Turkey");
 
-#region FirstExample
 var turkey = new Turkey();
-turkey.Cook();
+await turkey.Cook(); // running on background thread
+
+Console.WriteLine("Turkey Completed");
+Console.WriteLine("Making Gravy");
+
+var gravy = new Gravy();
+await gravy.Cook();  // running on background thread
+
+Console.WriteLine("Gravy Completed");
+Console.WriteLine("Ready to Eat");
+
 #endregion
-
-
-#region Callbacks
-turkey.Cook()
-    .ContinueWith(_ =>
-    {
-        var gravy = new Gravy();
-        gravy.Cook()
-            .ContinueWith(_ =>
-            {
-                Console.WriteLine("Gravy Completed");
-                Console.WriteLine("Ready to eat");
-            });
-        
-    });
-#endregion
-
-Console.ReadLine();
