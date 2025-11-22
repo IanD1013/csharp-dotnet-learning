f9b675d8c698ffff48e59dcfd03a815759b434fe
IanD1013 <ian.tung1013@gmail.com>
2025-11-23 10:31:12 +1300

    Behavioral - command - implement the command design pattern

---
+++ course-design-patterns/behavioral/Command/Command.cs
@@ -0,0 +1,7 @@
+﻿namespace Command;
+
+public interface Command
+{
+    void Execute();
+    void Undo();
+}
---
+++ course-design-patterns/behavioral/Command/Program.cs
@@ -0,0 +1,15 @@
+﻿using Command;
+
+Receiver receiver = new();
+Command.Command command1 = new ConcreteCommand(receiver, "1");
+Command.Command command2 = new ConcreteCommand(receiver, "2");
+Command.Command command3 = new ConcreteCommand(receiver, "3");
+
+Invoker invoker = new();
+invoker.AddCommand(command1);
+invoker.AddCommand(command2);
+invoker.AddCommand(command3);
+
+invoker.ExecuteCommands();
+invoker.UndoLastCommand();
+invoker.UndoLastCommand();
