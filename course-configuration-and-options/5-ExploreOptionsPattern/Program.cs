f7cdd318c06198a6b751d14e307aa47eac3e4fe4
IanD1013 <ian.tung1013@gmail.com>
2025-12-11 16:01:17 +1300

    Options pattern - consume logging options through DI

--- course-configuration-and-options/5-ExploreOptionsPattern/Program.cs
+++ course-configuration-and-options/5-ExploreOptionsPattern/Program.cs
@@ -1,4 +1,5 @@
 using _5_ExploreOptionsPattern.Logging;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,29 +21,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5)
-            .Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapGet(pattern: "/logging/options",
+        handler: static (IOptions<LoggingOptions> options) =>
+        {
+            var loggingOptions = options.Value;
+            
+            return Results.Json(data: loggingOptions);
+        })
+    .WithName("GetLoggingOptions")
+    .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
