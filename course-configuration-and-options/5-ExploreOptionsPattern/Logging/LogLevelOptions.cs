21e5402ef130c063538421c6896886621434cbfb
IanD1013 <ian.tung1013@gmail.com>
2025-12-11 15:13:45 +1300

    Options pattern - model the logging options

---
+++ course-configuration-and-options/5-ExploreOptionsPattern/Logging/LogLevelOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace _5_ExploreOptionsPattern.Logging;
+
+public sealed class LogLevelOptions : Dictionary<string, LogLevel>
+{
+    public LogLevelOptions() : base(StringComparer.OrdinalIgnoreCase)
+    {
+    }
+}
---
+++ course-configuration-and-options/5-ExploreOptionsPattern/Logging/LoggingOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace _5_ExploreOptionsPattern.Logging;
+
+public sealed class LoggingOptions
+{
+    // The logging configuration section name. This is exposed by convention
+    // to allow consumers of these options to bind configuration to their 
+    // named-section
+    public const string LoggingConfigurationSectionName = "Logging";
+    
+    public LogLevelOptions? LogLevel { get; set; }
+}
