d231d3e64be60f07756f15af0ec2e9f12ececea8
IanD1013 <ian.tung1013@gmail.com>
2025-12-11 17:21:31 +1300

    Options pattern - binding additional named-options

--- course-configuration-and-options/5-ExploreOptionsPattern/Program.cs
+++ course-configuration-and-options/5-ExploreOptionsPattern/Program.cs
@@ -1,14 +1,26 @@
+using _5_ExploreOptionsPattern.Features;
 using _5_ExploreOptionsPattern.Logging;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Bind IOptions<LoggingOptions> to the "Logging" config section for DI
 builder.Services.AddOptions<LoggingOptions>()
     .Bind(config: builder.Configuration.GetSection(
         key: LoggingOptions.LoggingConfigurationSectionName));
 
+// Add named configuration options
+builder.Services.AddOptions<FeatureOptions>(
+        name: "TodoApi")
+    .Bind(config: builder.Configuration.GetSection(
+        key: "Features:TodoApi"));
+
+builder.Services.AddOptions<FeatureOptions>(
+        name: "WeatherStation")
+    .Bind(config: builder.Configuration.GetSection(
+        key: "Features:WeatherStation"));
+
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -25,7 +37,7 @@ app.MapGet(pattern: "/logging/options",
         handler: static (IOptions<LoggingOptions> options) =>
         {
             var loggingOptions = options.Value;
-            
+
             return Results.Json(data: loggingOptions);
         })
     .WithName("GetLoggingOptions")
