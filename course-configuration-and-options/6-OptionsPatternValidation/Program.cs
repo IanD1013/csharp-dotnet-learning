379c44a11c280fea3cf0373923fbce457b740eab
IanD1013 <ian.tung1013@gmail.com>
2025-12-12 16:23:21 +1300

    Options pattern validation - register options with validation

--- course-configuration-and-options/6-OptionsPatternValidation/Program.cs
+++ course-configuration-and-options/6-OptionsPatternValidation/Program.cs
@@ -1,13 +1,19 @@
 using _6_OptionsPatternValidation;
 using _6_OptionsPatternValidation.Features;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddOptions<FeatureOptions>(name: "TodoApi")
-    .BindConfiguration(configSectionPath: "Features:TodoApi");
+    .BindConfiguration(configSectionPath: "Features:TodoApi")
+    .ValidateOnStart();
 
-builder.Services.AddOptions<FeatureOptions>(name: "WeatherStation")
+builder.Services.AddOptionsWithValidateOnStart<FeatureOptions>(name: "WeatherStation")
     .BindConfiguration(configSectionPath: "Features:WeatherStation");
+
+builder.Services.TryAddEnumerable(
+    descriptor: ServiceDescriptor.Singleton<IValidateOptions<FeatureOptions>, FeatureOptions>());
 
 builder.Services.AddHostedService<Worker>();
 
