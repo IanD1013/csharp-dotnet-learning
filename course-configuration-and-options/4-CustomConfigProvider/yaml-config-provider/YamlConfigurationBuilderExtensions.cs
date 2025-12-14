cc0eea3257276a334095fa1296e2041cd2d7bd55
IanD1013 <ian.tung1013@gmail.com>
2025-12-15 11:52:59 +1300

    Implement a custom configuration provider

---
+++ course-configuration-and-options/4-CustomConfigProvider/yaml-config-provider/YamlConfigurationBuilderExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+
+namespace yaml_config_provider;
+
+public static class YamlConfigurationBuilderExtensions
+{
+    public static IConfigurationBuilder AddYamlFile(
+        this IConfigurationBuilder builder, string path) =>
+        AddYamlFile(
+            builder,
+            provider: null,
+            path: path,
+            optional: false,
+            reloadOnChange: false);
+
+    public static IConfigurationBuilder AddYamlFile(
+        this IConfigurationBuilder builder, string path, bool optional) =>
+        AddYamlFile(
+            builder,
+            provider: null,
+            path: path,
+            optional: optional,
+            reloadOnChange: false);
+
+    public static IConfigurationBuilder AddYamlFile(
+        this IConfigurationBuilder builder, string path, bool optional, bool reloadOnChange) =>
+        AddYamlFile(
+            builder,
+            provider: null,
+            path: path,
+            optional: optional,
+            reloadOnChange: reloadOnChange);
+
+    public static IConfigurationBuilder AddYamlFile(
+        this IConfigurationBuilder builder,
+        IFileProvider? provider,
+        string path,
+        bool optional,
+        bool reloadOnChange)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentException.ThrowIfNullOrWhiteSpace(path);
+
+        return builder.AddYamlFile(s =>
+        {
+            s.FileProvider = provider;
+            s.Path = path;
+            s.Optional = optional;
+            s.ReloadOnChange = reloadOnChange;
+            s.ResolveFileProvider();
+        });
+    }
+
+    public static IConfigurationBuilder AddYamlFile(
+        this IConfigurationBuilder builder,
+        Action<YamlConfigurationSource>? configureSource) =>
+        builder.Add(configureSource);
+}
---
+++ course-configuration-and-options/4-CustomConfigProvider/yaml-config-provider/YamlConfigurationProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace yaml_config_provider;
+
+public sealed class YamlConfigurationProvider(
+    YamlConfigurationSource source) : FileConfigurationProvider(source)
+{
+    public override void Load(Stream stream)
+    {
+        try
+        {
+            Data = YamlConfigurationFileParser.Parse(stream);
+        }
+        catch (Exception ex)
+        {
+            throw new FormatException("Unable to parse YAML.", ex);
+        }
+    }
+}
