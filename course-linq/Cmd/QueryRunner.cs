0c4fe3717bd3fcce887b155657c737d98bc07b62
IanD1013 <ian.tung1013@gmail.com>
2025-12-16 18:06:38 +1300

    linq - filtering & ordering - intro to demo solution

---
+++ course-linq/Cmd/QueryRunner.cs
@@ -0,0 +1,23 @@
+using Data.Repositories;
+
+namespace Cmd;
+
+public abstract class QueryRunner
+{
+    protected readonly MarvelMovieRepository Repository = new();
+
+    public abstract void Run();
+
+    protected void Print<T>(T objectToPrint)
+    {
+        Console.WriteLine(objectToPrint);
+    }
+    
+    protected void PrintAll<T>(IEnumerable<T> objectsToPrint)
+    {
+        foreach (var objectToPrint in objectsToPrint)
+        {
+            Console.WriteLine(objectToPrint);
+        }
+    }
+}
