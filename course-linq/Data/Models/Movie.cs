54dae44cb9bc874826aa0b29e1940f54de1dc0f1
IanD1013 <ian.tung1013@gmail.com>
2025-12-17 12:07:31 +1300

    linq - deal with partial results - use Skip, Take and TakeWhile to retrieve a specific result

--- course-linq/Data/Models/Movie.cs
+++ course-linq/Data/Models/Movie.cs
@@ -2,9 +2,10 @@ namespace Data.Models;
 
 public class Movie
 {
-    public required Guid MovieId { get; set; }
-    public required string Name { get; set; }
-    public required DateOnly ReleaseDate { get; set; }
+    public required Guid MovieId { get; init; }
+    public required string Name { get; init; }
+    public required DateOnly ReleaseDate { get; init; }
+    public int Phase { get; init; }
     public List<Person> Directors { get; set; } = [];
     public List<Person> Producers { get; set; } = [];
 
