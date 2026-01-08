f0c266ad738e13f56fed0c40a62195345fbe0a19
IanD1013 <ian.tung1013@gmail.com>
2026-01-09 10:03:26 +1300

    Rest api - use the HttpClientFactory

--- course-rest-api-dotnet/Movies.Api.Sdk.Consumer/Program.cs
+++ course-rest-api-dotnet/Movies.Api.Sdk.Consumer/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
 using Movies.Contracts.Requests;
 using Refit;
 
-var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+// var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
 
+var services = new ServiceCollection();
+
+services.AddRefitClient<IMoviesApi>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:5001"));
+
+var provider = services.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
+
+// GET 
 var movie = await moviesApi.GetMovieAsync("nick-the-greek-2022");
 Console.WriteLine(JsonSerializer.Serialize(movie));
 
-
+// GET ALL
 var request = new GetAllMoviesRequest
 {
     Title = null,
