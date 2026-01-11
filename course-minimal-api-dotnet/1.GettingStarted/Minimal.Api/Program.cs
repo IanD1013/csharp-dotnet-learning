88e8c22de1b78702a05541c9db78bb238342b059
IanD1013 <ian.tung1013@gmail.com>
2026-01-11 23:13:28 +1300

    Minimal api - getting started - the different return types

--- course-minimal-api-dotnet/1.GettingStarted/Minimal.Api/Program.cs
+++ course-minimal-api-dotnet/1.GettingStarted/Minimal.Api/Program.cs
@@ -4,4 +4,18 @@ var app = builder.Build();
 app.MapGet("get-example", () => "Hello from GET");
 app.MapPost("post-example", () => "Hello from POST");
 
+app.MapGet("ok-object", () => Results.Ok(new
+{
+    Name = "Ian Dong"
+}));
+
+app.MapGet("slow-request", async () =>
+{
+    await Task.Delay(5000);
+    return Results.Ok(new
+    {
+        Name = "Ian Dong"
+    });
+});
+
 app.Run();
