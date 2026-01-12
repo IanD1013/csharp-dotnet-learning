0ddacc678fd3e048b4d35cb90e8a5d489cab55ec
IanD1013 <ian.tung1013@gmail.com>
2026-01-12 22:09:09 +1300

    Minimal api - getting started - available response types

--- course-minimal-api-dotnet/1.GettingStarted/Minimal.Api/Program.cs
+++ course-minimal-api-dotnet/1.GettingStarted/Minimal.Api/Program.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Minimal.Api;
 
@@ -15,7 +16,8 @@ var app = builder.Build();
 app.MapGet("get-example", () => "Hello from GET");
 app.MapPost("post-example", () => "Hello from POST");
 
-/** THE DIFFERENT RETURN TYPES: **/
+#region The Different Return Types
+
 app.MapGet("ok-object", () => Results.Ok(new
 {
     Name = "Ian Dong"
@@ -30,7 +32,11 @@ app.MapGet("slow-request", async () =>
     });
 });
 
-/** ROUTING REQUESTS: **/
+#endregion
+
+
+#region Routing Requests
+
 app.MapGet("get", () => "This is a GET");
 app.MapPost("post", () => "This is a POST");
 app.MapPut("put", () => "This is a PUT");
@@ -43,12 +49,21 @@ var handler = () => "This is coming from a var";
 app.MapGet("handler", handler);
 app.MapGet("from-class", Example.SomeMethod);
 
+#endregion
+
+
+#region Route Parameters and Rules
+
 /** ROUTE PARAMETERS AND RULES: **/
 app.MapGet("get-params/{age:int}", (int age) => $"Age provided was  {age}");
 app.MapGet("cars/{carId:regex(^[a-z0-9]+$)}", (string carId) => $"Car id provided was {carId}");
 app.MapGet("books/{isbn:length(13)}", (string isbn) => $"ISBN provided was {isbn}");
 
-/** PARAMETER BINDING: **/
+#endregion
+
+
+#region Parameter Binding
+
 // searchTerm is from query param by convention
 app.MapGet("people/search", (string? searchTerm, PeopleService peopleService) =>
 {
@@ -68,10 +83,14 @@ app.MapGet("mix/{routeParam}", (
         [FromHeader(Name = "Accept-Encoding")] string encoding) =>
     $"{routeParam}-{queryParam}-{guidGenerator.NewGuid}-{encoding}");
 
-// this implies that person was from body
+// this implies that person was from body because of "Post"
 app.MapPost("people", ([FromBody] Person person) => Results.Ok(person));
 
-/** SPECIAL PARAMETER TYPES **/
+#endregion
+
+
+#region Special Parameter Types
+
 // there are some special parameter types that are automatically bound if so you need them
 app.MapGet("httpcontext-1", async context => { await context.Response.WriteAsync("Hello from HttpContext 1"); });
 
@@ -91,11 +110,40 @@ app.MapGet("claims", (ClaimsPrincipal user) => { });
 
 app.MapGet("cancel", (CancellationToken token) => { });
 
-/** CUSTOM PARAMETER BINDING **/
-app.MapGet("map-point", (MapPoint point) => Results.Ok(point));
+#endregion
 
+
+#region Custom Parameter Binding
+
+app.MapGet("map-point", (MapPoint point) => Results.Ok(point));
 app.MapPost("map-point", (MapPoint point) => Results.Ok(point));
 
+#endregion
+
+
+#region Available Response Types
+
+app.MapGet("simple-string", () => "Hello world");
+app.MapGet("json-raw-obj", () => new { message = "Hello world" });
+app.MapGet("ok-obj", () => Results.Ok(new { Message = "Hello world" }));
+app.MapGet("json-obj", () => Results.Json(new { message = "Hello world" }));
+app.MapGet("text-string", () => Results.Text("Hello world"));
+
+app.MapGet("stream-result", () =>
+{
+    var memoryStream = new MemoryStream();
+    var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8);
+    streamWriter.Write("Hello world");
+    streamWriter.Flush();
+    memoryStream.Seek(0, SeekOrigin.Begin);
+
+    return Results.Stream(memoryStream);
+});
+
+app.MapGet("redirect", () => Results.Redirect("https://google.com"));
+app.MapGet("download", () => Results.File("./my-file.txt"));
+
+#endregion
 
 // Middleware registration stops here
 app.Run();
