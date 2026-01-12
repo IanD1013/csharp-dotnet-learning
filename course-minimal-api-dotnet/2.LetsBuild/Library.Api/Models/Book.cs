b93977efb55e1a80560c8d34f49a8c005e23ba50
IanD1013 <ian.tung1013@gmail.com>
2026-01-13 10:57:23 +1300

    Minimal api - let's build - project setup and database creation

---
+++ course-minimal-api-dotnet/2.LetsBuild/Library.Api/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Api.Models;
+
+public class Book
+{
+    public string Isbn { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public string ShortDescription { get; set; } = null!;
+    public int PageCount { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
