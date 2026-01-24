6544dcc3a5ba01ebb675a5106739dee336c6eaa8
IanD1013 <ian.tung1013@gmail.com>
2026-01-24 14:20:38 +1300

    First source generator - get started

---
+++ course-source-generators/Demo/Demo.Generator/ToJsonGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Demo.Generator;
+
+[Generator]
+public class ToJsonGenerator : IIncrementalGenerator
+{
+    private const string HelloWorld =
+        """
+        namespace Demo.Generator;
+
+        public class HelloWorld
+        {
+        }
+        """;
+
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(ctx => ctx
+            .AddSource("HelloWorld.g.cs", SourceText.From(HelloWorld, Encoding.UTF8)));
+    }
+}
