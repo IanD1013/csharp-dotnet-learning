0eb5a769319aeeed8a4a9c103c7c98e73d25da26
IanD1013 <ian.tung1013@gmail.com>
2026-01-26 22:05:13 +1300

    Trees and models - the transform method

--- course-source-generators/Demo/Demo.Generator/ToJsonGenerator.cs
+++ course-source-generators/Demo/Demo.Generator/ToJsonGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
@@ -26,9 +27,42 @@ public class ToJsonGenerator : IIncrementalGenerator
         context.RegisterPostInitializationOutput(ctx => ctx
             .AddSource("ToJsonSerializerAttribute.g.cs", SourceText.From(ToJsonSerializerAttribute, Encoding.UTF8)));
 
-        var provider = context.SyntaxProvider.CreateSyntaxProvider((node, token) => node is ClassDeclarationSyntax
+        var provider = context.SyntaxProvider.CreateSyntaxProvider(Predicate, Transform);
+    }
+
+    private static ClassInfo? Transform(GeneratorSyntaxContext syntaxContext, CancellationToken cancellationToken)
+    {
+        var classDeclarationSyntax = (ClassDeclarationSyntax)syntaxContext.Node;
+
+        foreach (var attributeList in classDeclarationSyntax.AttributeLists)
+        {
+            foreach (var attributeSyntax in attributeList.Attributes)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return null;
+                }
+                
+                var attributeName = attributeSyntax.Name.ToString();
+                if (attributeName != "ToJsonSerializerAttribute" && attributeName != "ToJsonSerializer")
+                {
+                    continue;
+                }
+            }
+        }
+        
+        return new ClassInfo();
+    }
+
+    private static bool Predicate(SyntaxNode node, CancellationToken token)
+    {
+        return node is ClassDeclarationSyntax
         {
             AttributeLists.Count: > 0
-        });
+        };
     }
+}
+
+public record struct ClassInfo  
+{
 }
