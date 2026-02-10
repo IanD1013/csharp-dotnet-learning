3a5ce25db04f292628716101a42bf2f639e99255
IanD1013 <ian.tung1013@gmail.com>
2026-02-10 21:31:58 +1300

    Application setup - create initial solution and projects

---
+++ course-modular-monoliths/RiverBooks.Web/Program.cs
@@ -0,0 +1,21 @@
+using RiverBooks.Books;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddBookServices();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapBookEndpoints();
+app.Run();
