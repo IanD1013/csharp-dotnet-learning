7bb405f3e6f3f467d8a539d8b7369556d7e2dc1a
IanD1013 <ian.tung1013@gmail.com>
2026-02-12 20:31:19 +1300

    The books module - integrate with EF core

---
+++ course-modular-monoliths/Note.md
@@ -0,0 +1,17 @@
+﻿under \RiverBooks.Web:
+```
+dotnet tool install --global dotnet-ef
+```
+```
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 8.0.8
+```
+```
+dotnet ef migrations add Initial 
+-c BookDbContext 
+-p ..\RiverBooks.Books\RiverBooks.Books.csproj 
+-s .\RiverBooks.Web.csproj
+-o Data/Migrations
+```
+```
+dotnet ef database update
+```
---
+++ course-modular-monoliths/RiverBooks.Books/BookConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace RiverBooks.Books;
+
+internal class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    internal static readonly Guid Book1Guid = new("00000000-0000-0000-0000-000000000001");
+    internal static readonly Guid Book2Guid = new("00000000-0000-0000-0000-000000000002");
+    internal static readonly Guid Book3Guid = new("00000000-0000-0000-0000-000000000003");
+
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.Property(b => b.Title)
+            .HasMaxLength(DataSchemaConstants.DEFAULT_NAME_LENGTH)
+            .IsRequired();
+
+        builder.Property(b => b.Author)
+            .HasMaxLength(DataSchemaConstants.DEFAULT_NAME_LENGTH)
+            .IsRequired();
+
+        builder.HasData(GetSampleBookData());
+    }
+
+    private IEnumerable<Book> GetSampleBookData()
+    {
+        var tolkien = "J.R.R. Tolkien";
+
+        yield return new Book(Book1Guid, "The Fellowship of the Ring", tolkien, 10.99m);
+        yield return new Book(Book2Guid, "The Two Towers", tolkien, 11.99m);
+        yield return new Book(Book3Guid, "The Return of the King", tolkien, 12.99m);
+    }
+}
