e8cb7ff76ee61511eef98858787131861fe265fa
IanD1013 <ian.tung1013@gmail.com>
2026-02-12 21:38:48 +1300

    Build the web api - add book endpoints - Delete

---
+++ course-modular-monoliths/RiverBooks.Books/DeleteBookEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using FastEndpoints;
+
+namespace RiverBooks.Books;
+
+internal class DeleteBookEndpoint(IBookService bookService) : Endpoint<DeleteBookRequest>
+{
+    private readonly IBookService _bookService = bookService;
+    
+    public override void Configure()
+    {
+        Delete("/books/{id}");
+        AllowAnonymous();
+    }
+    
+    public override async Task HandleAsync(DeleteBookRequest req, CancellationToken ct)
+    {
+        await _bookService.DeleteBookAsync(req.Id);
+        await SendNoContentAsync();
+    }
+}
