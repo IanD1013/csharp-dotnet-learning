d18b7247198e610d00709f70ed79f33169659517
IanD1013 <ian.tung1013@gmail.com>
2026-02-15 22:20:23 +1300

    The users module - create the users module

---
+++ course-modular-monoliths/Users Module/RiverBooks.Users/UsersDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Reflection;
+
+namespace RiverBooks.Users;
+
+public class UsersDbContext : IdentityDbContext
+{
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("Users");
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<decimal>()
+            .HavePrecision(18, 6);
+    }
+}
