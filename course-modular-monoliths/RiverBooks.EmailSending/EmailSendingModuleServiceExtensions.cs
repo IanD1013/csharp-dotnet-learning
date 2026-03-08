e90a8f8ba558026e35a5b5f0afbc27e0b2b220ae
IanD1013 <ian.tung1013@gmail.com>
2026-03-08 19:53:23 +1300

    The email sending module - introduce the EmailSending module - initial setup and service registrations

---
+++ course-modular-monoliths/RiverBooks.EmailSending/EmailSendingModuleServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace RiverBooks.EmailSending;
+
+public static class EmailSendingModuleServiceExtensions
+{
+    public static IServiceCollection AddEmailSendingModuleServices(
+        this IServiceCollection services,
+        ConfigurationManager config,
+        ILogger logger,
+        List<System.Reflection.Assembly> mediatRAssemblies)
+    {
+        // if using MediatR in this module, add any assemblies that contain handlers to the list
+        mediatRAssemblies.Add(typeof(EmailSendingModuleServiceExtensions).Assembly);
+
+        logger.Information("{Module} module services registered", "Email Sending");
+        return services;
+    }
+}
