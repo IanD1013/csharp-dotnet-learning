eb7bf56eb6e7dbd6a829da63539df523766b980b
IanD1013 <ian.tung1013@gmail.com>
2026-03-08 20:15:23 +1300

    The email sending module - introduce the EmailSending module - have basics of email sending module put together

--- course-modular-monoliths/RiverBooks.EmailSending/EmailSendingModuleServiceExtensions.cs
+++ course-modular-monoliths/RiverBooks.EmailSending/EmailSendingModuleServiceExtensions.cs
@@ -12,6 +12,9 @@ public static class EmailSendingModuleServiceExtensions
         ILogger logger,
         List<System.Reflection.Assembly> mediatRAssemblies)
     {
+        // Add module services
+        services.AddTransient<ISendEmail, MimeKitEmailSender>(); 
+        
         // if using MediatR in this module, add any assemblies that contain handlers to the list
         mediatRAssemblies.Add(typeof(EmailSendingModuleServiceExtensions).Assembly);
 
